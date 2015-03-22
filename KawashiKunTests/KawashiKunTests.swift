a8fdbb44a912c9d473596af5fd8211f5bfdc9f57
山口 翔馬 <it_bothered_what_i@yamagucumanombp.lan>
2015-03-22 18:30:51 +0900

    Initial Commit

---
+++ KawashiKunTests/KawashiKunTests.swift
@@ -0,0 +1,36 @@
+//
+//  KawashiKunTests.swift
+//  KawashiKunTests
+//
+//  Created by 山口 翔馬 on 2015/03/22.
+//  Copyright (c) 2015年 yamaguchi. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class KawashiKunTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
